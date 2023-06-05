d33c52ff25a3a2ea6d4178900209ec59c66afc24
Kactuswow <bdalvarezg@ufpso.edu.co>
2023-06-05 13:18:42 -0500

    Componentes Vue y TailwindCSS

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<script setup>
+  // import CardComponent from './components/CardComponent.vue';
+  import CardComponent from '@/components/CardComponent.vue';
+  import { ref } from 'vue';
+  const users = ref([
+  { 
+    name: 'Floopa',
+    Desc: 'Waos'
+  },
+  {
+    name: 'Sus',
+    Desc: 'Waos*2'
+  }])
+
+  const borrartarjeta = function(indice) {
+
+    users.value.splice(indice, 1)
+
+  }
+
+  let name = ref("");
+  let Desc = ref("");
+
+
+  const Agregar = function() {
+
+    let Extra = {
+      name: name.value,
+      Desc: Desc.value
+    }
+
+    users.value.push(Extra)
+    name.value = "";
+    Desc.value = "";
+
+  }
+
+</script>
+
+<template>
+
+  <div class="Card-Wrap flex w-[900px] gap-5 flex-wrap mx-auto">
+
+    <CardComponent v-for="(user, indice) in users" :key="indice" :name="user.name" :Desc = "user.Desc" :indice  = "indice" @borrartarjeta="borrartarjeta"></CardComponent>
+
+  </div>
+
+  <div class="Leer flex flex-col gap-5 mx-auto w-[900px] items-center justify-center mt-10 bg-[#ededed] p-5 rounded-lg">
+
+    <label for="">Nombre</label>
+    <input type="text" class="px-[10px] outline-none" v-model="name">
+    <label for="">Descripción</label>
+    <input type="text" class="px-[10px] outline-none" v-model="Desc">
+
+    <button @click="Agregar" class="border-2 px-[70px] py-[10px] bg-white rounded-lg">subir</button>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/CardComponent.vue
@@ -0,0 +1,54 @@
+<script setup>
+
+const Mensaje = function() {
+
+    alert("Curioso");
+
+}
+
+const borrartarjeta = function(indice) {
+
+    emit('borrartarjeta', indice)
+
+}
+
+const emit = defineEmits(['borrartarjeta'])
+
+const props = defineProps(
+    {
+      name: {
+        type: String,
+        default: 'no name'
+      },
+      Desc: {
+        type: String,
+        default: 'no Desc'
+      },
+      indice: {
+        type: Number
+      }
+    }
+  )
+
+</script>
+
+<template>
+
+    <div class="Card flex flex-col items-center bg-[#ededed] w-[350px] mx-auto p-5 gap-5 rounded-xl transition-[1s] hover:shadow-xl relative mt-10">
+
+        <span @click="Mensaje" class="imageProfile w-[300px] rounded-lg overflow-hidden cursor-pointer">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Big_Floppa_and_Justin_2_%28cropped%29.jpg/1127px-Big_Floppa_and_Justin_2_%28cropped%29.jpg">
+        </span>
+
+        <div class="info text-center">
+
+            <h1 class="text-[30px]">{{ props.name }}</h1>
+            <p>{{ props.Desc }}</p>
+
+        </div>
+
+        <span class="absolute top-3 right-3 bg-[#bbb] text-[25px] w-[50px] h-[50px] text-center rounded-full flex justify-center items-center cursor-pointer" @click="borrartarjeta(props.indice)">X</span>
+
+    </div>
+
+</template>
